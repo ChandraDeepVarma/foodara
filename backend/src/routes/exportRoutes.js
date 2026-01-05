464653b9ec19f542a33ea57476d00b268e364e13
Chandra Deep Varma <namburichandradeep123@gmail.com>
2026-01-05 12:32:32 +0530

    secure export-db with auth middleware

--- backend/src/routes/exportRoutes.js
+++ backend/src/routes/exportRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { exportDatabase } from "../controllers/exportController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/export-db", exportDatabase);
+router.post("/export-db", authMiddleware, exportDatabase);
 
 export default router;
